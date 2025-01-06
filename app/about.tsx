30de0b2d957c82a78be9da46b7a79171aa6964da
Sagar Bhandari <rsaagar7200@gmail.com>
2025-01-06 17:08:34 +0545

    created expo react native app and added firt page and navigating between page using expo router link and stack

---
+++ app/about.tsx
@@ -0,0 +1,43 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { Link } from 'expo-router'
+
+const about = () => {
+  return (
+    <View style={styles.container}>
+    <Text style={styles.text}>About screen</Text>
+    <Link href={'/'} style={styles.button}>
+    <Text >Go to Back</Text>
+    </Link>
+  </View>
+  )
+}
+
+export default about
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+       paddingTop: 20
+      },
+      text: {
+        color: '#25292e',
+        fontSize: 24,
+        fontWeight: 'bold',
+      },
+      button: {
+        fontSize: 16,
+        textDecorationLine: 'none',
+        color: '#25292e',
+        boxShadow: '4px -4px 4px rgba(0, 0, 0, 0.25)',
+        borderWidth:1,
+        borderColor:'#25292e',
+        borderRadius: 10,
+        padding:15, 
+        marginTop:'auto',
+        marginBottom:'auto'
+      },
+})
---
+++ app/index.tsx
@@ -0,0 +1,40 @@
+import { Text, View, StyleSheet } from 'react-native';
+ import { Link } from 'expo-router'; 
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Home screen</Text>
+      <Link href={"/about"} style={styles.button}>
+        Go to About screen
+      </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+   paddingTop: 20
+  },
+  text: {
+    color: '#25292e',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  button: {
+    fontSize: 16,
+    textDecorationLine: 'none',
+    color: '#25292e',
+    boxShadow: '4px -4px 4px rgba(0, 0, 0, 0.25)',
+    borderWidth:1,
+    borderColor:'#25292e',
+    borderRadius: 10,
+    padding:15, 
+    marginTop:'auto',
+    marginBottom:'auto'
+  },
+});
